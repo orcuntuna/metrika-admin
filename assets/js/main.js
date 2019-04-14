7a55fb0f78b10eb4422d9ce5bdf99c3d9cde53de
orcuntuna <turboindirblog@gmail.com>
2019-04-14 20:41:25 +0300

    navbar submenu

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,17 @@ $(() => {
         $("#user-box").toggle(300); 
     });
 
+    $(".nav li.sub a").click(function(){
+        $(this).parent().find(".sub-nav").toggle();
+        $(this).parent().find(".sub-nav-close").toggle();
+        var navbarHeight = $(".nav").height();
+        $(this).parent().find(".sub-nav").css({"height": navbarHeight});
+        $(this).parent().find(".sub-nav-close").css({"height": navbarHeight});
+    });
+
+    $(".sub-nav-close").click(function(){
+        $(".sub-nav").hide();
+        $(".sub-nav-close").hide();
+    });
+
 });
